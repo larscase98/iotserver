f683c17bebfc382f3b2cb9d87fcb4787c87d8e26
Lars Case <lcase2@u.rochester.edu>
2020-11-15 13:34:47 -0700

    added notice on /api hit

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
 
+router.use("/", (req, res, next) => {
+  console.log("API endpoint request received.");
+
+  next();
+});
+
 router.get("/", (req, res) => {
   res.send(new Date());
 });
